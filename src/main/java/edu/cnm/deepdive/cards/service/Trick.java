44aee9a2afd615a5b612aac7b987439ea611aede
Chris Hughes <chris@speedycomputing.com>
2026-02-04 14:31:27 -0700

    custom comparators moved to comparator chain

--- src/main/java/edu/cnm/deepdive/cards/service/Trick.java
+++ src/main/java/edu/cnm/deepdive/cards/service/Trick.java
@@ -46,21 +46,11 @@ public class Trick {
       }
     }
 
-    blackPile.sort((card1, card2) -> {
-      int result = card1.getColor().compareTo(card2.getColor());
-      if (result == 0) {
-        result = card1.compareTo(card2);
-      }
-      return result;
-    });
+    blackPile.sort(
+        Comparator.comparing(Card::getColor).thenComparing(Comparator.naturalOrder()));
 
-    redPile.sort((card1, card2) -> {
-      int result = card2.getColor().compareTo(card1.getColor());
-      if (result == 0) {
-        result = card1.compareTo(card2);
-      }
-      return result;
-    });
+    redPile.sort(
+        Comparator.comparing(Card::getColor, Comparator.reverseOrder()).thenComparing(Comparator.naturalOrder()));
   }
 
   public void reveal() {
